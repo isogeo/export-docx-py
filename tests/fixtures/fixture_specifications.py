2b17712761a344e04fabcd56d3fdac04b75e4993
Guts <Guts@users.noreply.github.com>
2019-09-05 14:40:44 +0200

    add tests for specifications formatting

---
+++ tests/fixtures/fixture_specifications.py
@@ -0,0 +1,44 @@
+fixture_specifications = [
+    {
+        "conformant": True,
+        "specification": {
+            "_id": "1a2b3c4d5e6f7g8h9i0j11k12l13m14n",
+            "_tag": "specification:isogeo:1a2b3c4d5e6f7g8h9i0j11k12l13m14n",
+            "name": "CNIG CC v2014",
+            "link": "http://cnig.gouv.fr/wp-content/uploads/2014/10/141002_Standard_CNIG_CC_diffusion.pdf",
+            "published": "2014-10-02T00:00:00",
+        },
+    },
+    {
+        "conformant": False,
+        "specification": {
+            "_id": "1a2b3c4d5e6f7g8h9i0j11k12l13m20z",
+            "_tag": "specification:1a2b3c4d5e6f7g8h9i0j11k12l13m20z:1a2b3c4d5e6f7g8h9i0j11k12l13m20z",
+            "name": "Spécification - GT",
+            "link": "https://www.isogeo.com",
+            "owner": {
+                "_id": "1a2b3c4d5e6f7g8h9i0j11k12l13m20z",
+                "_tag": "owner:1a2b3c4d5e6f7g8h9i0j11k12l13m20z",
+                "_created": "2019-01-30T17:39:21.8947459+00:00",
+                "_modified": "2019-08-05T13:55:03.9109327+00:00",
+                "contact": {
+                    "_id": "azerty7g8h9i0j11k12l13m20z",
+                    "_tag": "contact:group:azerty7g8h9i0j11k12l13m20z",
+                    "_deleted": False,
+                    "type": "group",
+                    "name": "Isogeo TEST - SDK Migration",
+                    "zipCode": "33140",
+                    "countryCode": "FR",
+                    "available": False,
+                },
+                "canCreateMetadata": True,
+                "canCreateLegacyServiceLinks": True,
+                "areKeywordsRestricted": False,
+                "hasCswClient": True,
+                "hasScanFme": False,
+                "keywordsCasing": "lowercase",
+                "metadataLanguage": "es",
+            },
+        },
+    },
+]
